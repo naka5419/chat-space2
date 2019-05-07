66082e51c94a69ed694050ef81ccc61e49b7a3d4
naka5419 <hnakai323@yahoo.co.jp>
2019-05-07 12:27:00 +0900

    add-generate-model-message

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #has_many :messages
   has_many :user_groups
   has_many :groups, through: :user_groups
+  has_many :messages
 end
