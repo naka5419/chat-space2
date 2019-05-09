7f2c01684bf21009ec84d6b0e73c1e6967cb42c1
naka5419 <hnakai323@yahoo.co.jp>
2019-05-09 16:14:08 +0900

    correct-indent

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
     @messages = @group.messages.includes(:user)
   end
 
-	def create
+  def create
 	  @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
