a6de77cf31d5a445e1709248d0d15e584bb967ed
naka5419 <hnakai323@yahoo.co.jp>
2019-05-17 10:59:31 +0900

    edit-s3-2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 # config valid only for current version of Capistrano
+set :linked_files, %w{ config/secrets.yml }
+
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
-
-set :linked_files, %w{ config/secrets.yml }
 
 lock '3.11.0'
 
